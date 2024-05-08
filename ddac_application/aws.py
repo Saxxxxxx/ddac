84efd086c65e149a4c3c6764812fe32d07352b22
holun <holun@imail.blueoceanit.com.my>
2024-05-08 23:51:43 +0800

    hi

--- ddac_application/aws.py
+++ ddac_application/aws.py
@@ -13,19 +13,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(BASE_DIR/'.env')
 
 auth = AWS4Auth(env('AWS_ACCESS_KEY_ID'), env('AWS_SECRET_ACCESS_KEY'), 'us-east-1', 'execute-api',session_token=env('AWS_SESSION_TOKEN'))
-api_url = 'https://62i9cvwa68.execute-api.us-east-1.amazonaws.com'
+
 
 class S3AWS():
     def upload_s3_to_api(upload_array_data):
         upload_array_data = json.dumps(upload_array_data)
-        api_url = api_url + '/upload_to_s3'
+        api_url = env('API_URL') + '/upload_to_s3'
         headers = {'Content-Type': 'application/json'}
         response = requests.post(api_url, headers=headers, data=upload_array_data,auth=auth)
         response.raise_for_status()  # Raise an exception if the API call fails
 
     def delete_s3_to_api(delete_array_data):
         delete_array_data = json.dumps(delete_array_data)
-        api_url = api_url + '/delete_to_s3'
+        api_url = env('API_URL') + '/delete_to_s3'
         headers = {'Content-Type': 'application/json'}
         response = requests.delete(api_url, headers=headers, data=delete_array_data,auth=auth)
         response.raise_for_status()  # Raise an exception if the API call fails
@@ -38,7 +38,7 @@ class S3AWS():
         }
         json_data = json.dumps(json_data)
         # Send the request with JSON data
-        api_url = api_url + '/update_to_s3'
+        api_url = env('API_URL') + '/update_to_s3'
         headers = {'Content-Type': 'application/json'}
         response = requests.put(api_url, headers=headers, data=json_data,auth=auth)
         response.raise_for_status()  # Raise an exception if the API call fails
